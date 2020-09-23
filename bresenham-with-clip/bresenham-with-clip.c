d675953cf4e8a88d60e9525f2bb8f9a7eab93be7
CNLohr <lohr85@gmail.com>
2020-09-23 04:26:18 -0400

    Bump code

--- bresenham-with-clip/bresenham-with-clip.c
+++ bresenham-with-clip/bresenham-with-clip.c
@@ -1,3 +1,6 @@
+//
+//XXX WARNING THIS FILE IS CURRENTLY BROKEN REVIEW LATER
+
 /*
 	This is my algorithm for using Bresenham's line algorithm with clipping.
 	This makes it possibel to speed along very quickly without having to check
@@ -38,9 +41,11 @@ void HandleDestroy(){}
 
 #define BRESEN_W 320
 #define BRESEN_H 240
+#define MARGIN_X 200
+#define MARGIN_Y 200
 
 int bx0, by0, bx1, by1;
-void BresenPixel( int x, int y )
+void drawPixelUnsafe( int x, int y )
 {
 	if( x < 0 || x >= BRESEN_W || y < 0 || y >= BRESEN_H )
 	{
@@ -49,7 +54,7 @@ void BresenPixel( int x, int y )
 	}
 	else
 	{
-		CNFGTackPixel( x + BRESEN_W/2, y+ BRESEN_H/2 );
+		CNFGTackPixel( x + MARGIN_X, y+ MARGIN_Y );
 		//CNFGTackPixel( x, y );
 	}
 }
@@ -57,12 +62,18 @@ void BresenPixel( int x, int y )
 void BresenhamDrawLineWithClip( int x0, int y0, int x1, int y1 )
 {
 //Tune this as a function of the size of your viewing window, line accuracy, and worst-case scenario incoming lines.
-#define FIXEDPOINT 65536
-#define FIXEDPOINTD2 (FIXEDPOINT/2)
+//#define BRESEN_W OLED_WIDTH
+//#define BRESEN_H OLED_HEIGHT
+#define FIXEDPOINT 16
+#define FIXEDPOINTD2 15
 	int dx = (x1-x0);
 	int dy = (y1-y0);
 	int sdx = (dx>0)?1:-1;
 	int sdy = (dy>0)?1:-1;
+	int yerrdiv = ( dx * sdx );  //dy, but always positive.
+	int xerrdiv = ( dy * sdy );  //dx, but always positive.
+	int yerrnumerator = 0;
+	int xerrnumerator = 0;
 	int x = x0;
 	int y = y0;
 
@@ -70,13 +81,6 @@ void BresenhamDrawLineWithClip( int x0, int y0, int x1, int y1 )
 	by0 = y0;
 	bx1 = x1;
 	by1 = y1;
-
-	int yerrdiv = ( dx * sdx );  //dy, but always positive.
-	int xerrdiv = ( dy * sdy );  //dx, but always positive.
-	int yerrnumerator;
-	int xerrnumerator;
-
-	//printf( "\n%d %d -> %d %d  (%d %d) (%d %d)\n", x, y, x1, y1, dx, dy, sdx, sdy );
 
 	if( x < 0 && x1 < 0 ) return;
 	if( y < 0 && y1 < 0 ) return;
@@ -102,13 +106,10 @@ void BresenhamDrawLineWithClip( int x0, int y0, int x1, int y1 )
 		}
 		if( dxA || xerrdiv <= yerrdiv )
 		{
-			yerrnumerator = ((dy * sdy) * FIXEDPOINT + yerrdiv/2) / yerrdiv;
+			yerrnumerator = (((dy * sdy)<<16) + yerrdiv/2) / yerrdiv;
 			if( dxA )
 			{
-				int yn = (yerrnumerator * dxA)*sdy;
-				yn += FIXEDPOINTD2 * sdy; //This "feels" right
-				yn /= FIXEDPOINT;
-				y += yn;
+				y += (((yerrnumerator * dxA) ) * sdy) >> FIXEDPOINT; //This "feels" right
 				//Weird situation - if we cal, and now, both ends are out on the same side abort.
 				if( y < 0 && y1 < 0 ) return;
 				if( y > BRESEN_H-1 && y1 > BRESEN_H-1 ) return;
@@ -131,13 +132,10 @@ void BresenhamDrawLineWithClip( int x0, int y0, int x1, int y1 )
 		}
 		if( dyA || xerrdiv > yerrdiv )
 		{
-			xerrnumerator = ((dx * sdx) * FIXEDPOINT + xerrdiv/2 ) / xerrdiv;
+			xerrnumerator = (((dx * sdx)<<16) + xerrdiv/2 ) / xerrdiv;
 			if( dyA )
 			{
-				int xn = (xerrnumerator*dyA)*sdx;
-				xn += FIXEDPOINTD2 * sdx; //This "feels" right.
-				xn /= FIXEDPOINT;
-				x += xn;
+				x += (((xerrnumerator*dyA) ) * sdx) >> FIXEDPOINT; //This "feels" right.
 				//If we've come to discover the line is actually out of bounds, abort.
 				if( x < 0 && x1 < 0 ) return;
 				if( x > BRESEN_W-1 && x1 > BRESEN_W-1 ) return;
@@ -147,7 +145,7 @@ void BresenhamDrawLineWithClip( int x0, int y0, int x1, int y1 )
 
 	if( x1 == x && y1 == y )
 	{
-		BresenPixel( x, y );
+		drawPixelUnsafe( x, y );
 		return;
 	}
 
@@ -182,9 +180,10 @@ void BresenhamDrawLineWithClip( int x0, int y0, int x1, int y1 )
 	//difficult to guarantee under all situations and may have weird edge cases.
 	//So, I've decided to stick this here.
 
+	printf( "%d %d -> %d %d\n", x, y, x1, y1 );
 	if( xerrdiv > yerrdiv )
 	{
-		int xerr = FIXEDPOINTD2;
+		int xerr = 1<<FIXEDPOINTD2;
 		if( x1 < 0 ) x1 = 0;
 		if( x1 > BRESEN_W-1) x1 = BRESEN_W-1;
 		x1 += sdx; //Tricky - make sure the "next" mark we hit doesn't overflow.
@@ -194,21 +193,21 @@ void BresenhamDrawLineWithClip( int x0, int y0, int x1, int y1 )
 
 		for( ; y != y1; y+=sdy )
 		{
-			BresenPixel( x, y );
+			drawPixelUnsafe( x, y );
 			xerr += xerrnumerator;
-			while( xerr >= FIXEDPOINT )
+			while( xerr >= (1<<FIXEDPOINT) )
 			{
 				x += sdx;
-				if( x == x1 ) goto abortline;
-				BresenPixel( x, y );
-				xerr -= FIXEDPOINT;
+				if( x == x1 ) return;
+				drawPixelUnsafe( x, y );
+				xerr -= 1<<FIXEDPOINT;
 			}
 		}
-		BresenPixel( x, y );
+		drawPixelUnsafe( x, y );
 	}
 	else
 	{
-		int yerr = FIXEDPOINTD2;
+		int yerr = 1<<FIXEDPOINTD2;
 
 		if( y1 < 0 ) y1 = 0;
 		if( y1 > BRESEN_H-1 ) y1 = BRESEN_H-1;
@@ -219,20 +218,18 @@ void BresenhamDrawLineWithClip( int x0, int y0, int x1, int y1 )
 
 		for( ; x != x1; x+=sdx )
 		{
-			BresenPixel( x, y );
+			drawPixelUnsafe( x, y );
 			yerr += yerrnumerator;
-			while( yerr >= FIXEDPOINT )
+			while( yerr >= 1<<FIXEDPOINT )
 			{
 				y += sdy;
-				if( y == y1 ) goto abortline;
-				BresenPixel( x, y );
-				yerr -= FIXEDPOINT;
+				if( y == y1 ) return;
+				drawPixelUnsafe( x, y );
+				yerr -= 1<<FIXEDPOINT;
 			}
 		}
-		BresenPixel( x, y );
+		drawPixelUnsafe( x, y );
 	}
-abortline:
-	;
 }
 
 int main()
@@ -246,46 +243,62 @@ int main()
 		CNFGClearFrame();
 		int i;
 		CNFGColor( 0x222244 );
-		CNFGTackSegment( BRESEN_W/2, BRESEN_H/2, SCREEN_W - BRESEN_W/2, BRESEN_H/2 );
-		CNFGTackSegment( SCREEN_W - BRESEN_W/2, BRESEN_H/2, SCREEN_W - BRESEN_W/2, SCREEN_H - BRESEN_H/2 );
-		CNFGTackSegment( SCREEN_W - BRESEN_W/2, SCREEN_H - BRESEN_H/2, BRESEN_W/2, SCREEN_H - BRESEN_H/2 );
-		CNFGTackSegment( BRESEN_W/2, SCREEN_H - BRESEN_H/2, BRESEN_W/2, BRESEN_H/2 );
-		for( i = 0; i < 1000; i++ )
-		{
-			int maxx = SCREEN_W*5 *(sin(fv/330.)+1);//BRESEN_W;
-			int maxy = SCREEN_H*5 *(cos(fv/100.)+1);//BRESEN_H;
-			int cx = cos(fv/356.)*300+300;
-			int cy = cos(fv/432.)*300+300;
-			if( maxx == 0 || maxy == 0 ) continue;
-#if 1
-			int x0 = (rand()%maxx)-maxx/2+SCREEN_W/2+cx;
-			int y0 = (rand()%maxy)-maxy/2+SCREEN_H/2+cy;
-			int x1 = (rand()%maxx)-maxx/2+SCREEN_W/2+cx;
-			int y1 = (rand()%maxy)-maxy/2+SCREEN_H/2+cy;
-#else
-			int x0 = SCREEN_W/2;
-			int y0 = SCREEN_H/2-10+fv;
-			int x1 = SCREEN_W/2-200;
-			int y1 = SCREEN_H/2;
-#endif		
-#if 0
-			x0 = 100;
-			y0 = 300;
-			y1 = 400;
-			x1 = (rand()%100) + 100;
-#endif
-#if 0
-			x0 = 397+BRESEN_W/2; y0 = 1+BRESEN_H/2; x1 = 244+BRESEN_W/2; y1 = 0+BRESEN_H/2;
-#endif
-			CNFGColor( 0xff00ff );
-			int dx = 0, dy = 0;
-			//for( dy = -1; dy <= 1; dy ++) for( dx = -1; dx <= 1; dx++ )
-			{
-				CNFGTackSegment( x0+dx, y0+dy, x1+dx, y1+dy );
-			}
+		CNFGTackSegment( MARGIN_X, MARGIN_Y, MARGIN_X + BRESEN_W, MARGIN_Y );
+		CNFGTackSegment( MARGIN_X + BRESEN_W, MARGIN_Y, MARGIN_X + BRESEN_W, MARGIN_Y + BRESEN_H );
+		CNFGTackSegment( MARGIN_X + BRESEN_W, MARGIN_Y + BRESEN_H, MARGIN_X, MARGIN_Y + BRESEN_H );
+		CNFGTackSegment( MARGIN_X, MARGIN_Y + BRESEN_H, MARGIN_X, MARGIN_Y );
 
+		//CNFGTackSegment( BRESEN_W/2, BRESEN_H/2, SCREEN_W - BRESEN_W/2, BRESEN_H/2 );
+		//CNFGTackSegment( SCREEN_W - BRESEN_W/2, BRESEN_H/2, SCREEN_W - BRESEN_W/2, SCREEN_H - BRESEN_H/2 );
+		//CNFGTackSegment( SCREEN_W - BRESEN_W/2, SCREEN_H - BRESEN_H/2, BRESEN_W/2, SCREEN_H - BRESEN_H/2 );
+		//CNFGTackSegment( BRESEN_W/2, SCREEN_H - BRESEN_H/2, BRESEN_W/2, BRESEN_H/2 );
+
+		if( 0 )
+		{
 			CNFGColor( 0x00ff00 );
-			BresenhamDrawLineWithClip( x0 - BRESEN_W/2, y0 - BRESEN_H/2, x1 - BRESEN_W/2, y1 - BRESEN_H/2 );
+			//x0=-18452, y0=-15580, x1=128, y1=63
+			//BresenhamDrawLineWithClip( -18452, -15580, 128, 63 );
+			BresenhamDrawLineWithClip( -25656, 31744, 128, 0 );
+		}
+		else
+		{
+			for( i = 0; i < 1000; i++ )
+			{
+				int maxx = SCREEN_W*5 *(sin(fv/330.)+1);//BRESEN_W;
+				int maxy = SCREEN_H*5 *(cos(fv/100.)+1);//BRESEN_H;
+				int x = cos(fv/356.)*300+300;
+				int y = cos(fv/432.)*300+300;
+				if( maxx == 0 || maxy == 0 ) continue;
+	#if 1
+				int x0 = (rand()%maxx)-maxx/2+SCREEN_W/2+x;
+				int y0 = (rand()%maxy)-maxy/2+SCREEN_H/2+y;
+				int x1 = (rand()%maxx)-maxx/2+SCREEN_W/2+x;
+				int y1 = (rand()%maxy)-maxy/2+SCREEN_H/2+y;
+	#else
+				int x0 = SCREEN_W/2;
+				int y0 = SCREEN_H/2-10+fv;
+				int x1 = SCREEN_W/2-200;
+				int y1 = SCREEN_H/2;
+	#endif		
+	#if 0
+				x0 = 100;
+				y0 = 300;
+				y1 = 400;
+				x1 = (rand()%100) + 100;
+	#endif
+	#if 0
+				x0 = 397+BRESEN_W/2; y0 = 1+BRESEN_H/2; x1 = 244+BRESEN_W/2; y1 = 0+BRESEN_H/2;
+	#endif
+				CNFGColor( 0xff00ff );
+				int dx = 0, dy = 0;
+				//for( dy = -1; dy <= 1; dy ++) for( dx = -1; dx <= 1; dx++ )
+				{
+					CNFGTackSegment( x0+dx, y0+dy, x1+dx, y1+dy );
+				}
+
+				CNFGColor( 0x00ff00 );
+				BresenhamDrawLineWithClip( x0 - MARGIN_X, y0 - MARGIN_Y, x1 - MARGIN_X, y1 - MARGIN_Y );
+			}
 		}
 		CNFGSwapBuffers();
 
