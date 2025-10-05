57513518e4e5c935881a2d33aa1911124c79a19a
cnlohr <lohr85@gmail.com>
2025-10-05 05:25:12 -0400

    Discovring more

--- fix32_fft/testphasor.c
+++ fix32_fft/testphasor.c
@@ -13,23 +13,33 @@ int main()
 	//  32768 = 0.7227 
 	//  16384 = 0.3554  scomp2 = 91217
 	//   8192 = 0.1770  scomp2 = 92315
-	int64_t emark = 757275091;
+	//
+	// for 32-bit only
+	// 757274467 = 0.25 omega scomp2 = 3013304584
+	// 151834210 = 0.05 omega
+	int64_t emark = 151834210;
 
 	// emark is something like:
 	// 65536×sqrt(1-cos(omega_per_step))
 
 	int rstop = 80;
 
-	int64_t scomp2 = 1062592792ULL*2; // halp.  What should this be?
+	int64_t scomp2 = 3036051499ULL; // halp.  What should this be?
+
+	double omega = 0.0;
 
 	// Similar to // https://amycoders.org/tutorials/sintables.html  (The last part of the last section)
 	for( i = 0; i <= rstop; i++ )
 	{
 		int emarkadjust = (((int64_t)a*emark)>>32);
 		b = b - emarkadjust;
-		printf( "%d %d\n", (a*scomp2*2)>>32, b );
+
+		int sval = (a*scomp2*2)>>32;
+		printf( "%d %d / %d %d (%d %d)\n", sval, b, (int)(sin(omega)*1073741824), (int)(cos(omega)*1073741824), (int)(sval-sin(omega)*1073741824), (int)(b-cos(omega)*1073741824) );
 		b = b - emarkadjust;
 		a = a + (((int64_t)b*emark)>>32);
+
+		omega += 0.05;
 	}
 }
 
